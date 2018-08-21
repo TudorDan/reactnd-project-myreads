237bff588998ffab2fc8df44ff199132c2c66929
TudorDan <tudor.ist@gmail.com>
2018-08-21 18:10:11 +0300

    ref: Rewrite Shelf as stateless functional component

--- src/Shelf.js
+++ src/Shelf.js
@@ -1,36 +1,33 @@
-import React, { Component } from 'react'
+import React from 'react'
 import PropTypes from 'prop-types'
 import Book from './Book'
 
-class Shelf extends Component {
+const Shelf = (props) => {
+	const { books, onShelfChange, title, filter } = props
 
-	static propTypes = {
-		books: PropTypes.array.isRequired,
-		onShelfChange: PropTypes.func.isRequired,
-		title: PropTypes.string,
-		filter: PropTypes.string
-	}
-
-	render() {
-		const { books, onShelfChange, title, filter } = this.props
-
-		return (
-			<div className="bookshelf">
-				<h2 className="bookshelf-title">{title}</h2>
-				{filter === 'none' && (
-					<p>These won't be shown here after refresh but you may find them again using the search page ...</p>
-				)}
-				<div className="bookshelf-books">
-					<ol className="books-grid">
-						{books.filter( (book) => book.shelf === filter)
-								.map( (b) => {
-									return <Book book={b} onShelfChange={onShelfChange} key={b.id}/>
-						})}
-					</ol>
-				</div>
+	return (
+		<div className="bookshelf">
+			<h2 className="bookshelf-title">{title}</h2>
+			{filter === 'none' && (
+				<p>These won't be shown here after refresh but you may find them again using the search page ...</p>
+			)}
+			<div className="bookshelf-books">
+				<ol className="books-grid">
+					{books.filter( (book) => book.shelf === filter)
+							.map( (b) => {
+								return <Book book={b} onShelfChange={onShelfChange} key={b.id}/>
+					})}
+				</ol>
 			</div>
-		)
-	}
+		</div>
+	)
+}
+
+Shelf.propTypes = {
+	books: PropTypes.array.isRequired,
+	onShelfChange: PropTypes.func.isRequired,
+	title: PropTypes.string,
+	filter: PropTypes.string
 }
 
 export default Shelf
