396dc5591e0fb56c680da04d9498b362f3f4443d
TudorDan <tudor.ist@gmail.com>
2018-08-21 18:04:12 +0300

    ref: Rewrite Book as stateless functional component

--- src/Book2.js
+++ src/Book2.js
@@ -1,24 +1,31 @@
-import React from 'react'
+import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 
-const Book = (props) => {
-	const { book, onShelfChange } = props
-	let thumb = book.imageLinks ? book.imageLinks.thumbnail : 'https://via.placeholder.com/128x193?text=No+image' /* fix missing thumb */
-	let auth = book.authors ? book.authors : [] /* fix missing authors */
-	
-	const color = (shelf) => {
+class Book extends Component {
+
+	static propTypes = {
+		book: PropTypes.object.isRequired,
+		onShelfChange: PropTypes.func.isRequired
+	}
+
+	/* changes control color by shelf */
+	color(shelf) {
 		if( shelf === 'currentlyReading' ) 	{ return '#f66' }
 		if( shelf === 'wantToRead' ) 			{ return '#66f' }
 		if( shelf === 'read' ) 					{ return '#6a5' }
 		if( shelf === 'none' ) 					{ return '#666' }
 	}
 
-	return (
-		<li>
+	render () {
+		const { book, onShelfChange } = this.props
+
+		let thumb = book.imageLinks ? book.imageLinks.thumbnail : 'https://via.placeholder.com/128x193?text=No+image' /* fix missing thumb */
+		let auth = book.authors ? book.authors : [] /* fix missing authors */
+		return <li>
 			<div className="book">
 				<div className="book-top">
 					<div className="book-cover" style={{width: 128, height: 193, backgroundImage: `url(${thumb})`}}></div>
-					<div className="book-shelf-changer" style={{backgroundColor: color(book.shelf)}}>
+					<div className="book-shelf-changer" style={{backgroundColor: this.color(book.shelf)}}>
 						<select onChange={e => onShelfChange(book, e.target.value)} defaultValue={book.shelf}>
 							<option value="move" disabled>Move to...</option>
 							<option value="currentlyReading">Currently Reading</option>
@@ -32,12 +39,7 @@ const Book = (props) => {
 				<div className="book-authors">{auth.map( (a,index) => (<span key={index}>{a}<br/></span>) )}</div>
 			</div>
 		</li>
-	)
-}
-
-Book.propTypes = {
-	book: PropTypes.object.isRequired,
-	onShelfChange: PropTypes.func.isRequired
+	}
 }
 
 export default Book
